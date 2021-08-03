14ab2cd69f0278e9a42061d4c7bf88283050cfd3
Adi1169 <adityahfjc17@gmail.com>
2021-08-03 22:42:10 +0530

    Updated UI

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,23 +4,21 @@ class TodoItemsController < ApplicationController
     def create
         @todo_item = @todo_list.todo_items.create(todo_item_params)
         redirect_to @todo_list
-   end
-
-   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id])
-    if @todo_item.destroy
-     flash[:success] = "Todo List item was deleted."
-    else
-     flash[:error] = "Todo List item could not be deleted."
     end
-    redirect_to @todo_list 
-   end
+
+    def destroy
+        @todo_list.destroy
+        respond_to do |format|
+        format.html { redirect_to root_url, notice: 'Todo list was successfully destroyed.' }
+        format.json { head :no_content }
+        end
+    end
 
    def complete
     @todo_item.update_attribute(:completed_at, Time.now)
     redirect_to @todo_list, notice: "Todo item completed"
    end
-   
+
    private
    
    def set_todo_list
